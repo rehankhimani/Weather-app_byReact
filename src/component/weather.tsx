176e3d76de506ca8afb9b6634cbaeaf6e2685c70
rehankhimani <muhammadrehanabdulqadir@gmail.com>
2025-06-17 23:41:04 -0700

    update

--- src/component/weather.tsx
+++ src/component/weather.tsx
@@ -36,23 +36,11 @@ export default function Weather() {
   useEffect(() => {
     fetchWeather();
   }, []);
-const getConditionIcon = (condition: string) => {
-  const conditionLower = condition.toLowerCase();
-
-  if (conditionLower.includes("cloud")) return "/Icons/cloudy.png";
-  if (conditionLower.includes("rain")) return "/Icons/rainy.png";
-  if (conditionLower.includes("sun") || conditionLower.includes("clear"))
-    return "/icons/sunny.png";
-  if (conditionLower.includes("snow")) return "/Icons/snow.png";
-  if (conditionLower.includes("storm")) return "/Icons/storm.png";
-
-  return "/Icons/default.png"; // fallback
-};
 
   const getBackground = () => {
     const condition = weatherData?.current.condition.text.toLowerCase() || "";
-    if (condition.includes("rain")) return "public/Icons/heavy-rain.jpg";
-    return "public/Icons/image1.jpg";
+    if (condition.includes("rain")) return "rain.jpg";
+    return "sunny.jpg";
   };
 
   return (
@@ -69,32 +57,24 @@ const getConditionIcon = (condition: string) => {
             onChange={(e) => setCity(e.target.value)}
           />
           <button onClick={fetchWeather}>
-            <img src="Icons/SearchIcon.png" alt="Search" />
+            <img src="/SearchIcon.png" alt="Search" />
           </button>
         </div>
 
-       {weatherData && (
-  <div className="weather-info">
-    <h1>{weatherData.current.temp_c}°C</h1>
-    <h3>{weatherData.location.name}</h3>
-    <p>
-      <img
-        src={getConditionIcon(weatherData.current.condition.text)}
-        alt="Condition Icon"
-        style={{ width: "30px", verticalAlign: "middle", marginRight: "8px" }}
-      />
-      {weatherData.current.condition.text}
-    </p>
-
+        {weatherData && (
+          <div className="weather-info">
+            <h1>{weatherData.current.temp_c}°C</h1>
+            <h3>{weatherData.location.name}</h3>
+            <p>{weatherData.current.condition.text}</p>
 
             <div className="info-cards">
               <div className="info-card">
-                <img src="Icons/weather.png" alt="Humidity" />
+                <img src="/weather.png" alt="Humidity" />
                 <p>{weatherData.current.humidity}%</p>
                 <span>Humidity</span>
               </div>
               <div className="info-card">
-                <img src="Icons/wind.png" alt="Wind" />
+                <img src="/wind.png" alt="Wind" />
                 <p>{weatherData.current.wind_kph} km/h</p>
                 <span>Wind</span>
               </div>
